35c60b26fdd734c58614713d11e5117b1d44b7e7
goeunleee <goeun3501@gmail.com>
2021-01-26 18:24:11 +0900

    commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// <App />> component: html 반환
+ReactDOM.render(
+
+  <App />
+  ,
+  document.getElementById('root')
+);
+
+
